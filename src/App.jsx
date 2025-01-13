3a23ee21658f7f9be7b333fcf1692a1b56eeb378
Dulran Hemjitha <dhemjitha@gmail.com>
2025-01-13 16:21:40 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Button from "./Button";
+import Greeting from "./Greeting";
+
+const App = () => {
+
+    return (
+        <>
+        <Greeting/>
+        <Button/>
+        </>
+    );
+
+}
+
+export default App;
---
+++ src/Greeting.jsx
@@ -0,0 +1,11 @@
+const Greeting = () => {
+    const name = "Dulran";
+    const age = 19;
+
+    return (
+        <h1>Hello, {name}! You are {age} years old.</h1>
+    );
+
+}
+
+export default Greeting
