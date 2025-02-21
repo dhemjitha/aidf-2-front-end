51a8f90160858d26e36b1e4c3fc7daee2746056e
Dulran Hemjitha <dhemjitha@gmail.com>
2025-02-21 13:27:22 +0530

    feat: session 8

--- src/components/HotelListings.jsx
+++ src/components/HotelListings.jsx
@@ -1,6 +1,6 @@
 import HotelCard from "./HotelCard";
 import LocationTab from "./LocationTab";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "./ui/button";
 import { getHotels } from "@/lib/api/hotels";
 
@@ -9,6 +9,9 @@ import { getHotels } from "@/lib/api/hotels";
 export default function HotelListings() {
 
   const [hotels, setHotels] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [error, setError] = useState("");
 
   const locations = ["ALL", "France", "Italy", "Australia", "Japan"]
 
@@ -22,18 +25,54 @@ export default function HotelListings() {
     return hotel.location.toLowerCase().includes(selectedLocation.toLowerCase());
   })
 
-  return (
+  useEffect(() => {
+    getHotels().then((hotels) => {
+      setHotels(hotels);
+    }).catch((error) => {
+      setIsError(true);
+      setError(error.message);
+    }).finally(() => {
+      setIsLoading(false);
+    })
+  }, [])
+
+  if (isLoading) {
+      return(
+      <section className="px-5 py-6 lg:py-16 lg:px-8">
+        <div className="mb-12">
+          <h2 className="text-3xl md:text-4xl font-bold mb-4">
+            Top trending hotels worldwide
+          </h2>
+  
+          <p className="text-lg text-muted-foreground">
+            Discover the most trending hotels worldwide for an unforgettable
+            experience.
+          </p>
+        </div>
+  
+        <div className="flex items-center gap-x-2">
+          {
+            locations.map((location, i) => {
+              return (<LocationTab key={i} selectedLocation={selectedLocation} name={location} onClick={handleSelectLocation} />)
+            })
+          }
+        </div>
+  
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-4 cursor-pointer">
+          <p>Loading...</p>
+        </div>
+  
+      </section>
+      );
+  }
+
+  if (isError) {
+    return(
     <section className="px-5 py-6 lg:py-16 lg:px-8">
       <div className="mb-12">
         <h2 className="text-3xl md:text-4xl font-bold mb-4">
           Top trending hotels worldwide
         </h2>
-
-        <Button onClick={async () => {
-          const hotels = await getHotels();
-          setHotels(hotels);
-        }}>Fetch Data</Button>
-
 
         <p className="text-lg text-muted-foreground">
           Discover the most trending hotels worldwide for an unforgettable
@@ -43,20 +82,47 @@ export default function HotelListings() {
 
       <div className="flex items-center gap-x-2">
         {
-          locations.map((location) => {
-            return (<LocationTab selectedLocation={selectedLocation} name={location} onClick={handleSelectLocation} />)
+          locations.map((location, i) => {
+            return (<LocationTab key={i} selectedLocation={selectedLocation} name={location} onClick={handleSelectLocation} />)
           })
         }
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-4 cursor-pointer">
+        <p className="text-red-500">Error loading hotels...</p>
+      </div>
 
+    </section>
+    );
+  }
+
+  return (
+    <section className="px-5 py-6 lg:py-16 lg:px-8">
+      <div className="mb-12">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4">
+          Top trending hotels worldwide
+        </h2>
+
+        <p className="text-lg text-muted-foreground">
+          Discover the most trending hotels worldwide for an unforgettable
+          experience.
+        </p>
+      </div>
+
+      <div className="flex items-center gap-x-2">
+        {
+          locations.map((location, i) => {
+            return (<LocationTab key={i} selectedLocation={selectedLocation} name={location} onClick={handleSelectLocation} />)
+          })
+        }
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-4 cursor-pointer">
         {
           filteredHotels.map((hotel) => {
             return (<HotelCard key={hotel._id} hotel={hotel} />)
           })
         }
-
       </div>
     </section>
   );
