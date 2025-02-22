e8d957c360d49035ca0996d59cb35959b8189385
Dulran Hemjitha <dhemjitha@gmail.com>
2025-02-23 00:12:48 +0530

    fixed: client side error handling

--- src/components/HotelListings.jsx
+++ src/components/HotelListings.jsx
@@ -3,6 +3,8 @@ import LocationTab from "./LocationTab";
 import { useState, useEffect } from "react";
 import { Button } from "./ui/button";
 import { getHotels } from "@/lib/api/hotels";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { AlertCircle } from "lucide-react";
 
 
 
@@ -37,19 +39,19 @@ export default function HotelListings() {
   }, [])
 
   if (isLoading) {
-      return(
+    return (
       <section className="px-5 py-6 lg:py-16 lg:px-8">
         <div className="mb-12">
           <h2 className="text-3xl md:text-4xl font-bold mb-4">
             Top trending hotels worldwide
           </h2>
-  
+
           <p className="text-lg text-muted-foreground">
             Discover the most trending hotels worldwide for an unforgettable
             experience.
           </p>
         </div>
-  
+
         <div className="flex items-center gap-x-2">
           {
             locations.map((location, i) => {
@@ -57,42 +59,46 @@ export default function HotelListings() {
             })
           }
         </div>
-  
+
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-4 cursor-pointer">
           <p>Loading...</p>
         </div>
-  
+
       </section>
-      );
+    );
   }
 
   if (isError) {
-    return(
-    <section className="px-5 py-6 lg:py-16 lg:px-8">
-      <div className="mb-12">
-        <h2 className="text-3xl md:text-4xl font-bold mb-4">
-          Top trending hotels worldwide
-        </h2>
+    return (
+      <section className="px-5 py-6 lg:py-16 lg:px-8">
+        <div className="mb-12">
+          <h2 className="text-3xl md:text-4xl font-bold mb-4">
+            Top trending hotels worldwide
+          </h2>
 
-        <p className="text-lg text-muted-foreground">
-          Discover the most trending hotels worldwide for an unforgettable
-          experience.
-        </p>
-      </div>
+          <p className="text-lg text-muted-foreground">
+            Discover the most trending hotels worldwide for an unforgettable
+            experience.
+          </p>
+        </div>
 
-      <div className="flex items-center gap-x-2">
-        {
-          locations.map((location, i) => {
-            return (<LocationTab key={i} selectedLocation={selectedLocation} name={location} onClick={handleSelectLocation} />)
-          })
-        }
-      </div>
+        <div className="flex items-center gap-x-2 mb-4">
+          {
+            locations.map((location, i) => {
+              return (<LocationTab key={i} selectedLocation={selectedLocation} name={location} onClick={handleSelectLocation} />)
+            })
+          }
+        </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-4 cursor-pointer">
-        <p className="text-red-500">Error loading hotels...</p>
-      </div>
+        <Alert variant="destructive">
+          <AlertCircle className="h-4 w-4" />
+          <AlertTitle>Error</AlertTitle>
+          <AlertDescription>
+            Error while fetching data...
+          </AlertDescription>
+        </Alert>
 
-    </section>
+      </section>
     );
   }
 
